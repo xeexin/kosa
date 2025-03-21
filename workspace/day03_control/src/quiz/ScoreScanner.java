b30a60f8bbc99164ca5db865c8a71dffb0fe4617
HEEJIN LIM <xeexin0331@gmail.com>
2025-03-21 14:46:41 +0900

    if문 : 문제 (학점 과락 처리)
    // kor, eng, com, name -> 총점tot, 평균avg 구하는 프로그램 작성
    		// 평균 60점 이상, 과목 당 40점 이상 이어야 합격입니다.

--- workspace/day03_control/src/quiz/ScoreScanner.java
+++ workspace/day03_control/src/quiz/ScoreScanner.java
@@ -11,12 +11,13 @@ public class ScoreScanner {
 
 		// 1.변수 선언
 		int kor, eng, com, tot;
+		char grade = ' ';
 		String name;
 		double avg = 0.0;
-		
+
 		// 2. 메시지 출력 및 입력
 		System.out.print("이름을 입력하세요 : ");
-		name = sc.nextLine(); // next() vs nextLine() 
+		name = sc.nextLine(); // next() vs nextLine()
 
 		System.out.print("국어 성적을 입력하세요 : ");
 		kor = sc.nextInt();
@@ -27,16 +28,27 @@ public class ScoreScanner {
 		System.out.print("컴퓨터 성적을 입력하세요 : ");
 		com = sc.nextInt();
 
-		
 		// 3. 계산(처리)
 		tot = kor + eng + com;
 		avg = tot / 3;
-		
+
+		if (avg >= 90)
+			grade = 'A';
+		else if (avg >= 80)
+			grade = 'B';
+		else if (avg >= 70)
+			grade = 'C';
+		else if (avg >= 60)
+			grade = 'A';
+		else
+			grade = 'F';
 
 		// 4. 결과 출력
 		System.out.println("[" + name + "님의 성적]");
 		System.out.println("국어 : " + kor + "\t영어 : " + eng + "\t컴퓨터 : " + com + "\n총점 : " + tot);
 		System.out.printf("평균 : %.2f ", avg);
+		System.out.println("\n학점 : " + grade);
+
 	}
 }
 
---
+++ workspace/day03_control/src/quiz/ScoreScanner2.java
@@ -0,0 +1,53 @@
+//*
+package quiz;
+
+import java.util.Scanner;
+
+public class ScoreScanner2 {
+	public static void main(String[] args) {
+		// kor, eng, com, name -> 총점tot, 평균avg 구하는 프로그램 작성
+		// 평균 60점 이상, 과목 당 40점 이상 이어야 합격입니다.
+
+		Scanner sc = new Scanner(System.in);
+
+		// 1.변수 선언
+		int kor, eng, com, tot;
+		String name;
+		double avg = 0.0;
+
+		// 2. 메시지 출력 및 입력
+		System.out.print("이름을 입력하세요 : ");
+		name = sc.nextLine(); // next() vs nextLine()
+
+		System.out.print("국어 성적을 입력하세요 : ");
+		kor = sc.nextInt();
+
+		System.out.print("영어 성적을 입력하세요 : ");
+		eng = sc.nextInt();
+
+		System.out.print("전산 성적을 입력하세요 : ");
+		com = sc.nextInt();
+
+		// 3. 계산(처리)
+		tot = kor + eng + com;
+		avg = tot / 3;
+
+		// 4. 결과 출력
+		System.out.println("\n[" + name + "님의 성적]");
+		System.out.println("국어 : " + kor + "\t영어 : " + eng + "\t전산 : " + com + "\n총점 : " + tot);
+		System.out.printf("평균 : %.2f\n", avg);
+		System.out.println("---------------결과---------------");
+		if (avg >= 60 && kor >= 40 && eng >= 40 && com >= 40) {
+			System.out.println("[합격]");
+		} else if (avg >= 60 && kor < 40)
+			System.out.println("[낙제] -과락 (국어 점수 : " + kor + ")");
+		else if (avg >= 60 && eng < 40)
+			System.out.println("[낙제] -과락 (영어 점수 : " + eng + ")");
+		else if (avg >= 60 && com < 40)
+			System.out.println("[낙제] -과락 (전산 점수 : " + com + ")");
+		else
+			System.out.println("[낙제] -평균 점수 60점 미만 (평균 점수 : " + avg + ")");
+	}
+}
+
+//*/
