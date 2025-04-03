14e5cf49921da6c0e8504a08174d731a78df12b1
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-03 11:53:22 +0900

    StringTokenizer

--- workspace/day11_API/src/ex08/utilPakage/StringTokenizerMain.java
+++ workspace/day11_API/src/ex08/utilPakage/StringTokenizerMain.java
@@ -1,3 +1,39 @@
+//package ex08.utilPakage;
+//
+//import java.util.StringTokenizer;
+//
+//public class StringTokenizerMain {
+//	public static void main(String[] args) {
+//		StringTokenizer token = new StringTokenizer("사과=10|초콜렛=3|샴페인=1", "=|", true);
+//		
+//		
+//		while (token.hasMoreTokens()) { //다음 요소가 있다면
+////			System.out.println(token.nextToken());
+//			String str = token.nextToken();
+//			
+//			if(str.equals("=")) System.out.print("\t");
+//			else if(str.equals("|")) System.out.print("\n");
+//			else System.out.println(str);
+//		}
+//
+//	}
+//}
+
+//package ex08.utilPakage;
+//
+//import java.util.StringTokenizer;
+//
+//public class StringTokenizerMain {
+//	public static void main(String[] args) {
+//		StringTokenizer token = new StringTokenizer("김연아/박태환/ 손연재/ 이휘/ 김지은| 박준 &공현진", "/");
+//				//new StringTokenizer("김연아 박태환 손연재 이휘 김지은 박준 공현진");
+//		
+//		while (token.hasMoreTokens()) {
+//			System.out.println(token.nextToken());
+//		}
+//
+//	}
+//}
 
 package ex08.utilPakage;
 
@@ -5,10 +41,21 @@ import java.util.StringTokenizer;
 
 public class StringTokenizerMain {
 	public static void main(String[] args) {
-		StringTokenizer token = new StringTokenizer("김연아 박태환 손연재 이휘 김지은 박준 공현진");
+		StringTokenizer token = new StringTokenizer("김연아/박태환/ 손연재/ 이휘/ 김지은| 박준 &공현진", "/");
+		// new StringTokenizer("김연아 박태환 손연재 이휘 김지은 박준 공현진");
+
 		while (token.hasMoreTokens()) {
 			System.out.println(token.nextToken());
 		}
 
+		
+		System.out.println("--------------------------");
+		
+		// split()이용하기
+		String str = "김연아/박태환/손연재/이휘/김지은| 박준 &공현진";
+		String[] ret = str.split("/");
+		for(int x=0; x<ret.length; x++) {
+			System.out.println(ret[x]);
+		}
 	}
 }
